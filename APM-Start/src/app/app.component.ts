e5767888c7232e64786b1f53a6b34d33eb01c4e7
yonghong.tan <yonghong.tan@avanade.com>
2021-11-26 15:13:26 +0900

    [add] product page

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+// import { ProductListComponent } from './products/product-list.component';
 
 @Component({
   selector: 'pm-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  title = 'Product Management System';
 }
